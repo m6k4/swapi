9b2116589e4bc05c7ba552c6e8b20bbf5f610eff
Zuzanna Olszewska <zolszewska@apptension.com>
2025-06-02 09:51:55 +0200

    replace env variable

--- vite.config.ts
+++ vite.config.ts
@@ -22,7 +22,7 @@ export default defineConfig(({ mode }) => {
   server: {
     proxy: {
       '/api': {
-        target: process.env.VITE_BASE_API,
+        target: "https://swapi.dev",
         changeOrigin: true,
         secure: false, 
       },
